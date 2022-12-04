631511f80d540d8818fb0e3844918545324a0d6e
unknown <tannerwlumsden@gmail.com>
2022-12-04 13:26:37 -0500

    added questions

---
+++ script.js
@@ -0,0 +1,18 @@
+var userChoice = 0;
+var questonCorrect = 0;
+var questionWrong = 0;
+var questionBlank = 0;
+var timer;
+var time = 120;
+var questions = [
+{
+	question: "Who was the first pokemon created?",
+	answers: {
+		a: "Bulbasaur",
+		b: "Rhydon",
+		c: "Mew",
+		d: "Pikachu",
+	},
+	
+}
+]
