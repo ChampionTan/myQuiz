57e8e9819e78ab923ed1c7186ce92fff9eeb79fd
unknown <tannerwlumsden@gmail.com>
2022-12-05 18:26:07 -0500

    fixed answers and added the start of a timer

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 var questions = document.querySelectorAll(".question");
+var gameOver = document.querySelector(".gameover");
+var timer = document.querySelector(".timer");
 var cursor = 0;
+var secondsLeft = 60;
 
-var displayQuestion = function () {
+var answers = ["b", "c", "d", "a", "c"];
+
+var displayNextQuestion = function () {
+	//for every individual question with question class
 	for (var question of questions) {
 		console.log(question);
 		if (question.dataset.index != cursor) {
@@ -14,18 +20,48 @@ var displayQuestion = function () {
 	}
 };
 
+var displayGameOver = function () {
+	console.log(gameOver);
+	if (gameOver.dataset.index != cursor) {
+		gameOver.style.display = "none";
+	}
+	else{
+		gameOver.style.display = "block";
+	}
+
+	}
+
+
 var advance = function(event) {
 	var element = event.target;
-
+	// if they clicked on a buttton in the question class
 	if (element.matches('.question button')) {
-		if (cursor < questions.length - 1) {
+		checkAnswer(element);
+		//the cursor advances once 
+		if (cursor < questions.length) {
 			cursor++;
 		}
-		displayQuestion();
+		displayNextQuestion();
+		displayGameOver();
 	}
 };
 
+var checkAnswer = function(element) {
+	//get user selected answer
+	var selectedAnswer = element.value;
+	//get the correct answer
+	var correctAnswer = answers[cursor];
+	//compare them
+	if (selectedAnswer != correctAnswer) {
+		//if wrong alert and time penalty
+		alert("INCORRECT! Penalty applied");
+	}
+	//if correct do nothing
+}
+
 document.addEventListener('click', advance);
 
-displayQuestion();
+displayNextQuestion();
+displayGameOver();
+
 
