d35dcfd2131c5ecddc1643984bde41c5228fe03d
unknown <tannerwlumsden@gmail.com>
2022-12-05 19:45:38 -0500

    started working on timer

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 var questions = document.querySelectorAll(".question");
 var gameOver = document.querySelector(".gameover");
 var timer = document.querySelector(".timer");
+var start = document.querySelector(".start");
 var cursor = 0;
 var secondsLeft = 60;
 
+function displayTimer() {
+	//display a timer with 60 secs
+	timer.textContent = "Time Left: " + secondsLeft;
+	//have timer start on the click of start button
+
+	//timer starts counting down
+	//when time hits 0 stop game
+	//calculate score
+}
+
 var answers = ["b", "c", "d", "a", "c"];
+
+var hideStart = function() {
+	console.log(start);
+	if (start.dataset.index != cursor) {
+	start.style.display = "none";
+	}
+	else{
+		start.style.display = "flex";
+	}
+};
 
 var displayNextQuestion = function () {
 	//for every individual question with question class
@@ -31,26 +52,40 @@ var displayGameOver = function () {
 
 	}
 
+var startTimer = function() {
+	setInterval(decrementTime(), 1000);
+	
+}
+var decrementTime = function() {
+	secondsLeft--;
+	displayTimer();
+}	
+
 
 var advance = function(event) {
 	var element = event.target;
+	if (element.matches('.start button')){
+		cursor++;
+		startTimer();
+		hideStart();
+		
+	}
 	// if they clicked on a buttton in the question class
 	if (element.matches('.question button')) {
 		checkAnswer(element);
 		//the cursor advances once 
-		if (cursor < questions.length) {
-			cursor++;
+		cursor++;
 		}
-		displayNextQuestion();
-		displayGameOver();
-	}
+	
+	displayNextQuestion();
+	displayGameOver();
 };
 
 var checkAnswer = function(element) {
 	//get user selected answer
 	var selectedAnswer = element.value;
 	//get the correct answer
-	var correctAnswer = answers[cursor];
+	var correctAnswer = answers[cursor - 1];
 	//compare them
 	if (selectedAnswer != correctAnswer) {
 		//if wrong alert and time penalty
@@ -61,7 +96,9 @@ var checkAnswer = function(element) {
 
 document.addEventListener('click', advance);
 
+hideStart();
 displayNextQuestion();
 displayGameOver();
+displayTimer();
 
 
